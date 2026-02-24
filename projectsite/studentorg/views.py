10818e403ca5df8fbb9a401247b93f33677c5039
Princess Heaven Rica <ricaheaven18@email.com>
2026-02-24 16:26:07 +0800

    bootstrap successful

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic.list import ListView
+from studentorg.models import Organization
+
+class HomePageView(ListView):
+    model = Organization
+    context_object_name = 'organizations'  # Renamed from 'home' for clarity
+    template_name = "includes/home.html"
+    paginate_by = 10
+    ordering = ['name']
