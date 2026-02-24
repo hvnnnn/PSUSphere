022486ac52f8281d2b6ed9172e33c6c2146c24aa
Princess Heaven Rica <ricaheaven18@email.com>
2026-02-25 01:01:15 +0800

    Add Organization feature works sucessfully

---
+++ projectsite/studentorg/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from django.forms import ModelForm
+from .models import Organization
+
+class OrganizationForm(ModelForm):
+    class Meta:
+        model = Organization
+        fields = "__all__"
